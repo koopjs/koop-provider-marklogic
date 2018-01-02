13c5331e31fc5e91f4c2d28d9db09134908153b4
jkerr <james.kerr@marklogic.com>
2018-01-02 14:51:59 -0500

    Fixed ordering and test cases to make sure the results are the same when run on different setups

--- src/test/java/LimitFieldTest.java
+++ src/test/java/LimitFieldTest.java
@@ -26,9 +26,9 @@ public class LimitFieldTest extends AbstractFeatureServiceTest{
             .body("globalIdFieldName", is(""))
             .body("hasZ", is(false))
             .body("hasM", is(false))
-            
+
             .body("spatialReference.wkid", is(4326))
-            
+
             .body("fields.size()", is(3))
             .body("fields[0].name", is("OBJECTID"))
             .body("fields[0].type", is("esriFieldTypeOID"))
@@ -44,21 +44,23 @@ public class LimitFieldTest extends AbstractFeatureServiceTest{
             .body("fields[2].editable", is(false))
             .body("fields[2].nullable", is(true))
             .body("fields[2].domain", IsNull.nullValue())
-            
+
             .body("features.size()", is(20))
-            .body("features[0].attributes.OBJECTID", is(54322))
-            .body("features[0].attributes.name", is("Moldova"))
-            .body("features[0].attributes.domain", is("0372.ua"))
-            .body("features[0].geometry.x", is(29))
-            .body("features[0].geometry.y", is(47))
-            .body("features[19].attributes.OBJECTID", is(19093))
-            .body("features[19].attributes.name", is("Iraq"))
-            .body("features[19].attributes.domain", is("1080thefan.com"))
-            .body("features[19].geometry.x", is(44))
-            .body("features[19].geometry.y", is(33))
-            
+
+            .body("features[0].attributes.OBJECTID", is(1))
+            .body("features[0].attributes.name", is("United Kingdom"))
+            .body("features[0].attributes.domain", is("rtbf.be"))
+            .body("features[0].geometry.x", is(-2))
+            .body("features[0].geometry.y", is(54))
+
+            .body("features[19].attributes.OBJECTID", is(43))
+            .body("features[19].attributes.name", is("Greece"))
+            .body("features[19].attributes.domain", is("candianews.gr"))
+            .body("features[19].geometry.x", is(22))
+            .body("features[19].geometry.y", is(39))
+
             .body("exceededTransferLimit", is(false))
         ;
     }
-	
+
 }
